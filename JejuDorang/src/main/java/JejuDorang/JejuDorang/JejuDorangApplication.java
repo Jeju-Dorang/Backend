7be04787f66dc2b6f606d5db3c05da01974e158e
choiseoji <choiseoji123@naver.com>
2024-06-27 20:33:17 +0900

    :wrench:chore: init

---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/JejuDorangApplication.java
@@ -0,0 +1,13 @@
+package JejuDorang.JejuDorang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuDorangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuDorangApplication.class, args);
+	}
+
+}
