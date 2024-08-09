433240493b2fbf677bb0db08d0a194580218ce5b
choiseoji <choiseoji123@naver.com>
2024-08-09 22:26:28 +0900

    :sparkles: feat: 댓글 작성 api 추가

---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/comment/controller/CommentController.java
@@ -0,0 +1,26 @@
+package JejuDorang.JejuDorang.comment.controller;
+
+import JejuDorang.JejuDorang.comment.service.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/posts/comment/{questionPostId}")
+    public ResponseEntity createComment(
+            @PathVariable Long questionPostId,
+            @RequestBody String content) {
+
+        commentService.createComment(questionPostId, content);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
