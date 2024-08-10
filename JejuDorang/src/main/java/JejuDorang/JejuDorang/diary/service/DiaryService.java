4f2018f4707f635d5039d6c76198c18c38795d5a
choiseoji <choiseoji123@naver.com>
2024-08-10 20:38:09 +0900

    :sparkles: feat: 스토리 상세 페이지 정보 반환하는 api 추가

--- JejuDorang/src/main/java/JejuDorang/JejuDorang/diary/service/DiaryService.java
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/diary/service/DiaryService.java
@@ -1,6 +1,7 @@
 package JejuDorang.JejuDorang.diary.service;
 
 import JejuDorang.JejuDorang.diary.data.Diary;
+import JejuDorang.JejuDorang.diary.dto.DiaryDetailResponse;
 import JejuDorang.JejuDorang.diary.dto.DiaryRequest;
 import JejuDorang.JejuDorang.diary.repository.DiaryRepository;
 import JejuDorang.JejuDorang.member.data.Member;
@@ -17,10 +18,10 @@ public class DiaryService {
 
     private final DiaryRepository diaryRepository;
 
+    // 일기 작성
     public void createDiary(DiaryRequest diaryRequest) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Member member = (Member) authentication.getPrincipal();
-
 
         Diary diary = Diary.builder()
                 .title(diaryRequest.getTitle())
@@ -32,5 +33,19 @@ public class DiaryService {
                 .build();
 
         diaryRepository.save(diary);
+    }
+
+    // 스토리의 일기 상세 정보를 보여줌
+    public DiaryDetailResponse getDiaryDetail(Long diaryId) {
+        Diary diary = diaryRepository.findById(diaryId)
+                .orElseThrow(()->new IllegalArgumentException("존재하지 않는 일기 글입니다 : " + diaryId));
+
+        DiaryDetailResponse response = new DiaryDetailResponse(
+                diary.getMember().getName(),
+                diary.getDate(),
+                diary.getImage(),
+                diary.getContent()
+        );
+        return response;
     }
 }
