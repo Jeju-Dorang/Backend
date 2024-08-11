40d515f84a1ae12cbaa057c6551514e85a6adf2a
kokomong2 <ko9603@naver.com>
2024-08-11 22:00:14 +0900

    :sparkles: feat: AchievementDto 관련 코드 추가

---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/achievement/dto/AchievementDto.java
@@ -0,0 +1,18 @@
+package JejuDorang.JejuDorang.achievement.dto;
+
+import JejuDorang.JejuDorang.achievement.enums.AchievementStatus;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class AchievementDto {
+	private final String achievementIcon;
+	private final String achievementName;
+	private final String achievementComment;
+	private final Long maxAchieve;
+	private final Long achievementCnt;
+	private final AchievementStatus achievementStatus;
+}
