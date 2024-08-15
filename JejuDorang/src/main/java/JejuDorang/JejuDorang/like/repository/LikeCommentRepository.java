5f7c20c6d70a78ebcad7bae89f1b9509b212041c
choiseoji <choiseoji123@naver.com>
2024-08-15 16:08:58 +0900

    :sparkles: feat: 좋아요 생성 추가

---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/like/repository/LikeCommentRepository.java
@@ -0,0 +1,7 @@
+package JejuDorang.JejuDorang.like.repository;
+
+import JejuDorang.JejuDorang.like.data.LikeComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeCommentRepository extends JpaRepository<LikeComment, Long> {
+}
