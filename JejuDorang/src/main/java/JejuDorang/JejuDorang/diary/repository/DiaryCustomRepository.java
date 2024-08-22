fd011bf6ab1d01a7bc84cd0e7580745a7f9d36b8
kokomong2 <ko9603@naver.com>
2024-08-22 16:00:01 +0900

    :sparkles: feat: 내 일기 비밀 여부 변경 코드 추가

--- JejuDorang/src/main/java/JejuDorang/JejuDorang/diary/repository/DiaryCustomRepository.java
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/diary/repository/DiaryCustomRepository.java
@@ -8,10 +8,12 @@ import JejuDorang.JejuDorang.diary.data.Diary;
 import JejuDorang.JejuDorang.diary.dto.DiaryIdDto;
 import JejuDorang.JejuDorang.diary.dto.DiaryListResponseDTO;
 import JejuDorang.JejuDorang.diary.dto.MyDiaryDetailResponseDto;
+import JejuDorang.JejuDorang.diary.enums.SecretType;
 
 @Repository
 public interface DiaryCustomRepository {
 	List<DiaryIdDto> findTop3ByMemberId(Long memberId);
 	List<DiaryListResponseDTO> findAllByMemberId(Long memberId);
 	MyDiaryDetailResponseDto findDiaryDetailByDiaryIdAndMemberId(Long diaryId, long id);
+	int updateDiarySecret(Long diaryId, long id, SecretType secret);
 }
