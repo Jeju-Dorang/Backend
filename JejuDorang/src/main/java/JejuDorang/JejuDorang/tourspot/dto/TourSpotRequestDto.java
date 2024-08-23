ea86a0dad7484e58905cd42ccbe8213927d09b87
choiseoji <choiseoji123@naver.com>
2024-08-24 02:24:21 +0900

    :sparkles: feat: 위치 정보를 담아올 request dto 생성

---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/tourspot/dto/TourSpotRequestDto.java
@@ -0,0 +1,14 @@
+package JejuDorang.JejuDorang.tourspot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TourSpotRequestDto {
+
+    private String mapX;
+    private String mapY;
+}
