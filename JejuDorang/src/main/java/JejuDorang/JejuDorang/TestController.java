eaf2ba1809208cf7f226e7400a023c2997f1e953
choiseoji <choiseoji123@naver.com>
2024-09-13 20:40:58 +0900

    :sparkles: feat: 임시 컨트롤러 생성

---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/TestController.java
@@ -0,0 +1,43 @@
+package JejuDorang.JejuDorang;
+
+import JejuDorang.JejuDorang.diary.dto.DiaryPublicResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @Value("${server.env}")
+    private String env;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+
+    @Value("${serverName}")
+    private String serverName;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> heatlthCheck() {
+        Map<String, String> responseData = new HashMap<>();
+
+        responseData.put("serverName", serverName);
+        responseData.put("serverAddress", serverAddress);
+        responseData.put("serverPort", serverPort);
+        responseData.put("env", env);
+
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+}
