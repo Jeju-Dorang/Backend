786ad6233cc85c87fefb88c2b10479817658ad41
kokomong2 <ko9603@naver.com>
2024-09-18 16:17:05 +0900

    :sparkles: feat: 크롤링 카테고리 가져오는 기능 추가

--- JejuDorang/src/main/java/JejuDorang/JejuDorang/crawling/service/CrawlingService.java
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/crawling/service/CrawlingService.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
 import JejuDorang.JejuDorang.crawling.WebDriverUtil;
+import JejuDorang.JejuDorang.lodging.dto.KaKaoCrawlingDto;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
@@ -26,9 +27,10 @@ public class CrawlingService {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 
-	public void searchKaKaoMap(String searchTerm) {
+	public KaKaoCrawlingDto searchKaKaoMap(String searchTerm) {
+		double rating = 0.0;
+		String category = "";
 		try {
-			searchTerm = removeBrackets(searchTerm);
 			driver.get("https://map.kakao.com/");
 
 			// 검색창 찾기 및 검색어 입력
@@ -48,43 +50,72 @@ public class CrawlingService {
 			// Thread.sleep(2000);
 
 			// 검색 결과 리스트에서 첫 번째 아이템의 별점 가져오기
-			getFirstPlaceRating();
-
+			rating = getFirstPlaceRating();
+			category = getFirstPlaceCategory();
 		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 			System.err.println("검색 중 인터럽트 발생: " + e.getMessage());
 		} catch (Exception e) {
 			System.err.println("검색 중 오류 발생: " + e.getMessage());
 		}
+		return new KaKaoCrawlingDto(rating, category);
 	}
 
 	public void close() {
 		WebDriverUtil.close(driver);
 	}
 
-	private void getFirstPlaceRating() {
+	private double getFirstPlaceRating() {
+		double rating = 0.0;
 		try {
 			if (isNoPlaceFound()) {
 				System.err.println("검색 결과가 없습니다.");
-				return;
+				return 0;
 			}
 
 			WebElement firstItem = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='info.search.place.list']/li[1]")));
 
 			if (firstItem == null) {
 				System.err.println("첫 번째 리스트 아이템을 찾지 못했습니다.");
-				return;
+				return 0;
 			}
 
 			WebElement ratingElement = firstItem.findElement(By.xpath(".//em[@data-id='scoreNum']"));
-			String rating = ratingElement.getText();
+			String ratingStr = ratingElement.getText();
+			rating = Double.parseDouble(ratingStr);
 
-			System.out.println("첫 번째 장소의 별점: " + rating);
-
+			System.out.println("첫 번째 장소의 별점: " + ratingStr);
 		} catch (Exception e) {
 			System.err.println("별점 정보를 가져오는 중 오류 발생: " + e.getMessage());
 		}
+		return rating;
 	}
+
+	private String getFirstPlaceCategory() {
+		String category = "";
+		try {
+			if (isNoPlaceFound()) {
+				System.err.println("검색 결과가 없습니다.");
+				return "";
+			}
+
+			WebElement firstItem = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='info.search.place.list']/li[1]")));
+
+			if (firstItem == null) {
+				System.err.println("첫 번째 리스트 아이템을 찾지 못했습니다.");
+				return "";
+			}
+
+			WebElement categoryElement = firstItem.findElement(By.xpath(".//span[@data-id='subcategory']"));
+			category = categoryElement.getText();
+
+			System.out.println("첫 번째 장소의 카테고리: " + category);
+		} catch (Exception e) {
+			System.err.println("카테고리 정보를 가져오는 중 오류 발생: " + e.getMessage());
+		}
+		return category;
+	}
+
 
 	private boolean isNoPlaceFound() {
 		try {
@@ -98,10 +129,6 @@ public class CrawlingService {
 			return false;
 		}
 		return false;
-	}
-
-	private String removeBrackets(String input) {
-		return input.replaceAll("\\(.*?\\)", "").trim();
 	}
 
 }
--- JejuDorang/src/main/java/JejuDorang/JejuDorang/lodging/enums/LodgingCategory.java
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/lodging/enums/LodgingCategory.java
@@ -14,4 +14,21 @@ public enum LodgingCategory {
 	LodgingCategory(String koreanName) {
 		this.koreanName = koreanName;
 	}
+
+	public static LodgingCategory valueOfCategory(String category) {
+		if (category.contains("호텔") || category.contains("리조트") || category.contains("호스텔") || category.contains("모텔") || category.contains("콘도")) {
+			return LodgingCategory.HOTEL;
+		}
+		else if (category.contains("게스트하우스")) {
+			return LodgingCategory.GUEST_HOUSE;
+		}
+		else if (category.contains("펜션")) {
+			return LodgingCategory.PENSION;
+		}
+		else if (category.contains("민박")) {
+			return LodgingCategory.BB;
+		}
+		else
+			return null;
+	}
 }
