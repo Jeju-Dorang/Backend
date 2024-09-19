e4d157ec2ba4ac8119016754399f8f684539b9dc
choiseoji <choiseoji123@naver.com>
2024-09-19 17:47:01 +0900

    :sparkles: feat: data.sql 추가

---
+++ JejuDorang/src/main/resources/META-INF/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (1, '알파카와 친해지길 바라🐐', '귀여운 동물 알파카 먹이주기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%8B%E1%85%A1%E1%86%AF%E1%84%91%E1%85%A1%E1%84%8F%E1%85%A1.png', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (2, '천지연 폭포 사진 찍기🌊', '천지연 폭포에서 사진찍자!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%91%E1%85%A9%E1%86%A8%E1%84%91%E1%85%A9.png', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (3, '10앨 연속 일기 달성🌟', '10일 연속 일기 달성!', 10, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/10%E1%84%8B%E1%85%B5%E1%86%AF%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%89%E1%85%A9%E1%86%A8.png', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (4, '질문글 답장 5개 달성✏️', '질문글 답장 5개 달기!', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/5%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (5, '제주 미니랜드 가기🌎', '제주 미니랜드 가기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%86%E1%85%B5%E1%84%82%E1%85%B5_%E1%84%85%E1%85%A2%E1%86%AB%E1%84%83%E1%85%B3.png', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (6, '제주 미로공원 가보기🧩', '제주 미로공원 탈출하기!', 1, 'url', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (7, '카트레이싱 해보기🚘', '카트 타보기!', 1, 'url', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (8, '질문글 답장 10개 달성✏️', '질문글 답장 10개 달기!', 10, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/10%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (9, '질문글 답장 20개 달성✏️', '질문글 답장 20개 달기!', 20, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/20%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (10, '만장굴 용암동굴 가기 🪨', '만장굴 용암동굴 가기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%83%E1%85%A9%E1%86%BC%E1%84%80%E1%85%AE%E1%86%AF.png', 'TOUR');
+
+INSERT INTO achievement (achievement_id, name, content, maxAchieve, image, category)
+VALUES (11, '제주도 승마체험🐴', '제주도 승마 체험 하기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%89%E1%85%B3%E1%86%BC%E1%84%86%E1%85%A1.png', 'TOUR');
