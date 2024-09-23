3be0e4cfcd3f14afdcbde4f430979a4c3d5d996d
SeungJun Go <105098279+kokomong2@users.noreply.github.com>
2024-09-23 22:26:16 +0900

    [feat] 메인페이지 api 추가 (#56)
    * :sparkles: feat: 업적, 숙소 데이터 추가
    
    * :sparkles: feat: 멤버, 캐릭터 데이터 관련 코드 추가
    
    * :sparkles: feat: 멤버, 캐릭터 데이터 관련 코드 추가
    
    * :sparkles: feat: 캐릭터 저장 기능 추가
    
    * :hammer: refactor: 데이터 수정
    
    * :hammer: refactor: dto accessLevel 삭제
    
    * :hammer: refactor: MemberMainResponseDto 어노테이션 추가
    
    * :hammer: refactor: getMainPage 쿼리문 수정
    
    * :hammer: refactor: getMainPage 쿼리문 수정
    
    * :hammer: refactor: getMainPage 쿼리문 수정

---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/character/dto/CharacterImageDto.java
@@ -0,0 +1,14 @@
+package JejuDorang.JejuDorang.character.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CharacterImageDto {
+	private int backgroundImage;
+	private int itemImage;
+	private int petImage;
+}
---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/lodging/dto/LodgingCoordinateDto.java
@@ -0,0 +1,13 @@
+package JejuDorang.JejuDorang.lodging.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LodgingCoordinateDto {
+	private double lat;
+	private double lng;  // 경도
+}
