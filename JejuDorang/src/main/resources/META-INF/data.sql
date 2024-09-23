a8b1655d267087f4b2a554f0eb06d1ed4a5ad3c6
kokomong2 <ko9603@naver.com>
2024-09-23 16:49:33 +0900

    :hammer: refactor: 데이터 수정

--- JejuDorang/src/main/resources/META-INF/data.sql
+++ JejuDorang/src/main/resources/META-INF/data.sql
@@ -47,31 +47,31 @@ VALUES ('한라산 백록담에서 사진찍기 ⛰️', '한라산 백록담에
 
 -- 숙소 관련 데이터
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('가름게스트하우스', '조용하고 아늑한 숙소', '', '제주특별자치도 서귀포시 법환하로9번길 10', 4.5, 33.2365870334, 126.5123383804, 50000, 0, 2);
+VALUES ('가름게스트하우스', '조용하고 아늑한 숙소', '', '제주특별자치도 서귀포시 법환하로9번길 10', 4.5, 33.2365870334, 126.5123383804, 50000, 'east', 'guestHouse');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('고망난돌민박', '성산일출봉 근처에 위치한 민박', '', '제주특별자치도 서귀포시 성산읍 일주동로 5464', 4.3, 33.3531185797, 126.8578558136, 60000, 3, 2);
+VALUES ('고망난돌민박', '성산일출봉 근처에 위치한 민박', '', '제주특별자치도 서귀포시 성산읍 일주동로 5464', 4.3, 33.3531185797, 126.8578558136, 60000, 'west', 'lodge');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('골드원호텔&스위트', '럭셔리한 호텔', '', '제주특별자치도 서귀포시 이어도로 1032 (서호동)', 4.8, 33.2442391264, 126.5223984479, 120000, 3, 0);
+VALUES ('골드원호텔&스위트', '럭셔리한 호텔', '', '제주특별자치도 서귀포시 이어도로 1032 (서호동)', 4.8, 33.2442391264, 126.5223984479, 120000, 'east', 'hotel');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('관음사가는길펜션', '관음사 입구 근처 펜션', 'http://tong.visitkorea.or.kr/cms/resource/96/2486096_image2_1.JPG', '제주특별자치도 제주시 산록북로 819 (아라일동)', 4.0, 33.4375488373, 126.5544690871, 70000, 1, 0);
+VALUES ('관음사가는길펜션', '관음사 입구 근처 펜션', 'http://tong.visitkorea.or.kr/cms/resource/96/2486096_image2_1.JPG', '제주특별자치도 제주시 산록북로 819 (아라일동)', 4.0, 33.4375488373, 126.5544690871, 70000, 'west', 'pension');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('광이멀스테이', '조용한 스테이', '', '제주특별자치도 제주시 월각로 151', 4.6, 33.4058844989, 126.3048803787, 80000, 2, 1);
+VALUES ('광이멀스테이', '조용한 스테이', '', '제주특별자치도 제주시 월각로 151', 4.6, 33.4058844989, 126.3048803787, 80000, 'south', 'lodge');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('귤익는마을', '자연 속에서 귤을 수확할 수 있는 숙소', 'http://tong.visitkorea.or.kr/cms/resource/68/1933168_image2_1.jpg', '제주특별자치도 서귀포시 성산읍 신천서로 11', 4.2, 33.3441111157, 126.8501205782, 90000, 1, 3);
+VALUES ('귤익는마을', '자연 속에서 귤을 수확할 수 있는 숙소', 'http://tong.visitkorea.or.kr/cms/resource/68/1933168_image2_1.jpg', '제주특별자치도 서귀포시 성산읍 신천서로 11', 4.2, 33.3441111157, 126.8501205782, 90000, 'south', 'lodge');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('그라벨호텔', '현대적 감각의 호텔', '', '제주특별자치도 제주시 일주서로 7316', 4.7, 33.4924157657, 126.4286648899, 150000, 0, 1);
+VALUES ('그라벨호텔', '현대적 감각의 호텔', '', '제주특별자치도 제주시 일주서로 7316', 4.7, 33.4924157657, 126.4286648899, 150000, 'south', 'hotel');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('그랜드 조선 제주', '제주의 고급 호텔', 'http://tong.visitkorea.or.kr/cms/resource/37/2819937_image2_1.jpg', '제주특별자치도 서귀포시 중문관광로72번길 60', 4.9, 33.2501588629, 126.4063028265, 200000, 2, 0);
+VALUES ('그랜드 조선 제주', '제주의 고급 호텔', 'http://tong.visitkorea.or.kr/cms/resource/37/2819937_image2_1.jpg', '제주특별자치도 서귀포시 중문관광로72번길 60', 4.9, 33.2501588629, 126.4063028265, 200000, 'north', 'hotel');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('그랜드 하얏트 제주', '제주에서 가장 큰 호텔 중 하나', 'http://tong.visitkorea.or.kr/cms/resource/83/2876783_image2_1.jpg', '제주특별자치도 제주시 노형동', 4.8, 33.4854284908, 126.4816104441, 180000, 3, 2);
+VALUES ('그랜드 하얏트 제주', '제주에서 가장 큰 호텔 중 하나', 'http://tong.visitkorea.or.kr/cms/resource/83/2876783_image2_1.jpg', '제주특별자치도 제주시 노형동', 4.8, 33.4854284908, 126.4816104441, 180000, 'north', 'hotel');
 
 INSERT INTO lodging (name, comment, image, address, rating, latitude, longitude, price, direction, category)
-VALUES ('그랜드메르호텔제주', '한적한 해변 근처에 위치한 호텔', '', '제주특별자치도 서귀포시 논짓물로 45 (하예동)', 4.6, 33.2396708139, 126.3861298105, 170000, 1, 3);
+VALUES ('그랜드메르호텔제주', '한적한 해변 근처에 위치한 호텔', '', '제주특별자치도 서귀포시 논짓물로 45 (하예동)', 4.6, 33.2396708139, 126.3861298105, 170000, 'north', 'hotel');
