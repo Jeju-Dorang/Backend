abc504a94878fb4bd7c3a8e1b0170b9878d2d38c
choiseoji <choiseoji123@naver.com>
2024-09-24 17:55:56 +0900

    :sparkles: feat: 필요한 어노테이션 추가

--- JejuDorang/src/main/java/JejuDorang/JejuDorang/character/data/Character.java
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/character/data/Character.java
@@ -4,17 +4,11 @@ import JejuDorang.JejuDorang.item.data.BackgroundItem;
 import JejuDorang.JejuDorang.item.data.PetItem;
 import JejuDorang.JejuDorang.item.data.StuffItem;
 import JejuDorang.JejuDorang.member.data.Member;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
