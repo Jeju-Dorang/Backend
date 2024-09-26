fe65510f24d8833611b6ed8aef3e535e7d2bab75
kokomong2 <ko9603@naver.com>
2024-09-26 16:49:51 +0900

    :hammer: refactor: achievementId 처리 관련 코드들 추가

--- JejuDorang/src/main/java/JejuDorang/JejuDorang/diary/dto/DiaryRequestDto.java
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/diary/dto/DiaryRequestDto.java
@@ -1,21 +1,25 @@
 package JejuDorang.JejuDorang.diary.dto;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import JejuDorang.JejuDorang.diary.enums.SecretType;
 import JejuDorang.JejuDorang.tag.dto.TagDto;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DiaryRequestDto {
 
+    @NotNull(message = "제목을 입력해주세요.")
     private String title;
 
+    @NotNull(message = "내용을 입력해주세요.")
     private String content;
 
     private String imageUrl;
@@ -23,6 +27,7 @@ public class DiaryRequestDto {
     @JsonProperty("secret")
     private SecretType secret;
 
+    @NotNull(message = "업적을 선택해주세요.")
     private Long achievementId;
 
     private List<TagDto> tagList;
