4cb083ac1b76a81b374b223b4407fdc063ebba6b
choiseoji <choiseoji123@naver.com>
2024-09-27 16:41:18 +0900

    :sparkles: feat: 일기 사진 s3에 저장하는 코드 작성

---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/config/S3Config.java
@@ -0,0 +1,28 @@
+package JejuDorang.JejuDorang.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        String accessKey = System.getenv("S3_ACCESS_KEY");
+        String secretKey = System.getenv("S3_SECRET_KEY");
+        String region = System.getenv("S3_REGION");
+
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+}
---
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/image/service/ImageService.java
@@ -0,0 +1,63 @@
+package JejuDorang.JejuDorang.image.service;
+
+import JejuDorang.JejuDorang.diary.data.Diary;
+import JejuDorang.JejuDorang.diary.repository.DiaryRepository;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final AmazonS3 amazonS3;
+    private final String bucketName = System.getenv("S3_BUCKET_NAME");
+
+    private final DiaryRepository diaryRepository;
+
+    private String changedImageName(String originName) {
+        String random = UUID.randomUUID().toString();
+        return random + originName;
+    }
+
+    // 이미지 업로드 메서드
+    public String uploadImage(MultipartFile imageFile, Long diaryId) throws IOException {
+        // 1. 고유한 파일 이름 생성
+        String originalFilename = imageFile.getOriginalFilename();
+        String changedName = changedImageName(originalFilename);
+
+        // 2. S3에 업로드할 파일의 메타데이터 설정
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(imageFile.getSize());
+        metadata.setContentType(imageFile.getContentType());
+
+        // 3. S3에 파일 업로드
+        amazonS3.putObject(bucketName, changedName, imageFile.getInputStream(), metadata);
+
+        // 4. 저장된 파일의 URL 반환
+        String storedUrl = amazonS3.getUrl(bucketName, changedName).toString();
+
+        //
+        Diary diary = diaryRepository.findById(diaryId)
+                .orElseThrow(()->new IllegalArgumentException("존재하지 않는 일기" ));
+        diary.updateImage(storedUrl);
+        diary.updateImageName(changedName);
+        diaryRepository.save(diary);
+
+        return storedUrl;
+    }
+
+    // Diary에 imageUrl 저장
+    public void saveImageInDiary(String url, Long diaryId) {
+
+        Diary diary = diaryRepository.findById(diaryId)
+                .orElseThrow(()->new IllegalArgumentException("존재하지 않는 일기" ));
+        diary.updateImage(url);
+        diaryRepository.save(diary);
+    }
+}
