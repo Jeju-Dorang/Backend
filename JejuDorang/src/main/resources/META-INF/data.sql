4f0fa445e122adea947439144cf217403ea3b899
kokomong2 <ko9603@naver.com>
2024-09-29 09:40:16 +0900

    :hammer: refactor: 데이터 수정

--- JejuDorang/src/main/resources/META-INF/data.sql
+++ JejuDorang/src/main/resources/META-INF/data.sql
@@ -7,25 +7,25 @@ INSERT INTO achievement (name, content, max_achieve, image, category)
 VALUES ('천지연 폭포 사진 찍기🌊', '천지연 폭포에서 사진찍자!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%91%E1%85%A9%E1%86%A8%E1%84%91%E1%85%A9.png', 'TOUR');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('10앨 연속 일기 달성🌟', '10일 연속 일기 달성!', 10, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/10%E1%84%8B%E1%85%B5%E1%86%AF%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%89%E1%85%A9%E1%86%A8.png', 'TOUR');
+VALUES ('10일 연속 일기 달성🌟', '10일 연속 일기 달성!', 10, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/10%E1%84%8B%E1%85%B5%E1%86%AF%E1%84%8B%E1%85%A7%E1%86%AB%E1%84%89%E1%85%A9%E1%86%A8.png', 'GENERAL');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('질문글 답장 5개 달성✏️', '질문글 답장 5개 달기!', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/5%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'TOUR');
+VALUES ('질문글 답장 5개 달성✏️', '질문글 답장 5개 달기!', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/5%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'GENERAL');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
 VALUES ('제주 미니랜드 가기🌎', '제주 미니랜드 가기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%86%E1%85%B5%E1%84%82%E1%85%B5_%E1%84%85%E1%85%A2%E1%86%AB%E1%84%83%E1%85%B3.png', 'TOUR');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('제주 미로공원 가보기🧩', '제주 미로공원 탈출하기!', 1, 'url', 'TOUR');
+VALUES ('제주 민속촌 가보기🏠', '제주 민속촌 가보기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%86%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A9%E1%86%A8%E1%84%8E%E1%85%A9%E1%86%AB.png', 'TOUR');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('카트레이싱 해보기🚘', '카트 타보기!', 1, 'url', 'TOUR');
+VALUES ('카트레이싱 해보기🚘', '카트 타보기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%8F%E1%85%A1%E1%84%90%E1%85%B3%E1%84%85%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%89%E1%85%B5%E1%86%BC.png', 'TOUR');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('질문글 답장 10개 달성✏️', '질문글 답장 10개 달기!', 10, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/10%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'TOUR');
+VALUES ('질문글 답장 10개 달성✏️', '질문글 답장 10개 달기!', 10, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/10%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'GENERAL');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('질문글 답장 20개 달성✏️', '질문글 답장 20개 달기!', 20, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/20%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'TOUR');
+VALUES ('질문글 답장 20개 달성✏️', '질문글 답장 20개 달기!', 20, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/20%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'GENERAL');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
 VALUES ('만장굴 용암동굴 가기 🪨', '만장굴 용암동굴 가기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%83%E1%85%A9%E1%86%BC%E1%84%80%E1%85%AE%E1%86%AF.png', 'TOUR');
@@ -34,15 +34,22 @@ INSERT INTO achievement (name, content, max_achieve, image, category)
 VALUES ('제주도 승마체험🐴', '제주도 승마 체험 하기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%89%E1%85%B3%E1%86%BC%E1%84%86%E1%85%A1.png', 'TOUR');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('딱새우 킬러🍤', '제주의 명물, 딱새우 먹기! 이정도 먹었으면 당신은 딱새우 킬러 ', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%84%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A2%E1%84%8B%E1%85%AE.png', 'FOOD');
+VALUES ('딱새우 킬러🍤', '제주의 명물, 딱새우 먹기!\n이정도 먹었으면 당신은 딱새우 킬러 ', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%84%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A2%E1%84%8B%E1%85%AE.png', 'FOOD');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('흑돼지 킬러🐷', '제주의 명물, 흑돼지 먹기! 이정도 먹었으면 당신은 흑돼지 킬러 ', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%92%E1%85%B3%E1%86%A8%E1%84%83%E1%85%AB%E1%84%8C%E1%85%B5.png', 'FOOD');
+VALUES ('흑돼지 킬러🐷', '제주의 명물, 흑돼지 먹기!\n이정도 먹었으면 당신은 흑돼지 킬러 ', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%92%E1%85%B3%E1%86%A8%E1%84%83%E1%85%AB%E1%84%8C%E1%85%B5.png', 'FOOD');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
-VALUES ('우도 땅콩아이스크림 킬러🍦', '우도의 명물, 땅콩 아이스크림 먹기! 이정도 먹었으면 아이스크림 중독', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%84%E1%85%A1%E1%86%BC%E1%84%8F%E1%85%A9%E1%86%BC.png', 'FOOD');
+VALUES ('우도 땅콩아이스크림 킬러🍦', '우도의 명물, 땅콩 아이스크림 먹기!\n이정도 먹었으면 아이스크림 중독', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%84%E1%85%A1%E1%86%BC%E1%84%8F%E1%85%A9%E1%86%BC.png', 'FOOD');
 
 INSERT INTO achievement (name, content, max_achieve, image, category)
 VALUES ('한라산 백록담에서 사진찍기 ⛰️', '한라산 백록담에서 사진찍기!\n이정도 했으면 당신은 전문 산악인', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%87%E1%85%A2%E1%86%A8%E1%84%85%E1%85%A9%E1%86%A8%E1%84%83%E1%85%A1%E1%86%B7.png', 'TOUR');
 
--- 숙소 관련 데이터
+INSERT INTO achievement (name, content, max_achieve, image, category)
+VALUES ('제주도 감귤 먹기🍊', '제주도 감귤 먹기!\n이정도 먹었으면 당신은 감귤 중독', 5, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%80%E1%85%A1%E1%86%B7%E1%84%80%E1%85%B2%E1%86%AF.jpg', 'FOOD');
+
+INSERT INTO achievement (name, content, max_achieve, image, category)
+VALUES ('테디베어뮤지엄제주 가기🧸', '테디베어뮤지엄제주 가기!\n이정도 가면 당신은 테디베어 마스터', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%90%E1%85%A6%E1%84%83%E1%85%B5%E1%84%87%E1%85%A6%E1%84%8B%E1%85%A5.png', 'TOUR');
+
+INSERT INTO achievement (name, content, max_achieve, image, category)
+VALUES ('첫 질문 글 작성하기📝', '첫 질문 글 작성하기!', 1, 'https://jejudorangs3.s3.ap-northeast-2.amazonaws.com/icon/%E1%84%8E%E1%85%A5%E1%86%BA%E1%84%8C%E1%85%B5%E1%86%AF%E1%84%86%E1%85%AE%E1%86%AB.png', 'GENERAL');
