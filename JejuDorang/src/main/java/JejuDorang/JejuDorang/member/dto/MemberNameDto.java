77d8b9af036ca19e1d4c0a26ac8bbbc818d6ea05
kokomong2 <ko9603@naver.com>
2024-10-01 16:20:25 +0900

    :sparkles: feat: requestDto valid 추가

--- JejuDorang/src/main/java/JejuDorang/JejuDorang/member/dto/MemberNameDto.java
+++ JejuDorang/src/main/java/JejuDorang/JejuDorang/member/dto/MemberNameDto.java
@@ -1,6 +1,7 @@
 package JejuDorang.JejuDorang.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 public class MemberNameDto {
 
 	@NotBlank(message = "이름을 입력해주세요.")
+	@NotNull(message = "이름을 입력해주세요.")
 	private String memberName;
 }
